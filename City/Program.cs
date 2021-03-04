22af0f0d2e78576b0a015e78dd8a982225bf0e3c
Vlad <vlad.but@emanuel.ro>
2021-03-04 09:05:17 +0200

    Add project files.

---
+++ City/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace City
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
